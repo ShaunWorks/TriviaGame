cda2f5f576426eebb50191533ee89e06b4c5b716
Torian <tharris@zyrobotics.com>
2018-12-19 12:16:28 -0500

    shuffle function added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,28 @@
+function shuffle(arr) {
+    let array = arr.slice(0);
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
 
 let game = {
+    playing: true,
     right: 0,
     wrong: 0,
+    questionsAnswered: 0,
+    questionLimit: 5,
     curQuestion: new triviaBlock(),
     questions: [],
     timer: null,
@@ -14,41 +35,50 @@ let game = {
 
     displayQuestion: function () {
         this.curQuestion = this.questions[Math.floor(Math.random() * this.questions.length)];
+        let options = shuffle(this.curQuestion.options);
         $("#quiz-title").text(this.curQuestion.title);
-        $("#quiz-answers").empty()
-        for (let i = 0; i < this.curQuestion.options.length; i++) {
-            $("#quiz-answers").append(`<li>${this.curQuestion.options[i]}`);
+        $("#quiz-answers").empty();
+        for (let i = 0; i < options.length; i++) {
+            $("#quiz-answers").append(`<li>${options[i]}`);
         };
+    },
+
+    displayAnswer: function () {
+        this.questionsAnswered++;
+        //console.log(game.questionsAnswered, game.questionLimit);
+        if (this.questionsAnswered === this.questionLimit) {
+            this.playing = false;
+            $("#quiz-answers").empty();
+            $("#quiz-title").html("Thanks for playing! Try again?")
+            $("#message").html(`Quiz Over`);
+
+        }
+        else {
+            setTimeout(function () {
+                game.displayQuestion();
+                $("#message").empty();
+            }, 3000)
+        }
     },
 
     answerCheck: function () {
         if ($(this).text() === game.curQuestion.answer) {
             //got right answer
             game.right++;
-            console.log("right:", game.right);
-            setTimeout(function() {
-                game.displayQuestion();
-            }, 3000)
-            console.log("this is the right answer");
+            $("#message").html("this is the right answer")
+
         }
         else {
             //got wrong answer
             game.wrong++;
-            console.log("wrong:", game.wrong);
-            setTimeout(function() {
-                game.displayQuestion();
-            }, 3000)
-            console.log("this is the wrong answer");
+            $("#message").html("this is the wrong answer")
         }
     },
-
-
 }
 
 $(document).ready(function () {
 
     game.fillQuestions();
     game.displayQuestion();
-
     $("ul").on("click", "li", game.answerCheck);
 })
